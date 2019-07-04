96d8298cfd95e0aefbb5b6cfb58be50896315930
Jagan <jagan.8981gmil.com>
2019-07-04 22:16:49 +0800

    GIT Test

---
+++ demo.py
@@ -0,0 +1,36 @@
+5print ("Hi am Jarvis")
+
+
+print("Enter your name:")
+x = str(input())
+print("Hello, " + x)
+
+
+print ("please Enter your Age:")
+y = input()
+
+print ("Thanks for the details")
+
+print ("To be suprised am a budding calculator program still learning to be a good one")
+
+print (" As of now am capable of doing addition")
+
+print ("let start the task for addition now")
+
+
+a = int(input("Enter a number: "))
+print ('hmm, please enter one more')
+b = int(input("Enter a number: "))
+print(a+b)
+print('yahhhhhh!!! am i right with the answer')
+
+print ("Thanks, " + x)
+
+print("Can you say Bye:")
+x = (input())
+print("Bye, " + "GoodBye")
+      
+exit ()
+
+
+
