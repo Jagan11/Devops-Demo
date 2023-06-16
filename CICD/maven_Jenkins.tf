236497afa0e7699be3728aa913caf9545ad5a4fe
Jaganathan Dhanasekar <jagan.8981@gmail.com>
2023-06-16 18:12:11 +0800

    maven

--- CICD/maven_Jenkins.tf
+++ CICD/maven_Jenkins.tf
@@ -1,11 +1,7 @@
-provider "null" {}
 resource "null_resource" "install_maven" {
   provisioner "remote-exec" {
     inline = [
-      "wget https://downloads.apache.org/maven/maven-3/3.8.2/binaries/apache-maven-3.8.2-bin.tar.gz",
-      "tar xzf apache-maven-3.8.2-bin.tar.gz",
-      "mv apache-maven-3.8.2 /opt/maven",
-      "echo 'export PATH=/opt/maven/bin:$PATH' >> ~/.bashrc",
+      "brew install maven",
     ]
   }
 }
