eea7218049154201b12bd45c80709ae294bdb732
Jaganathan Dhanasekar <jagan.8981@gmail.com>
2023-06-16 18:17:49 +0800

    MVN_update

--- CICD/maven_Jenkins.tf
+++ CICD/maven_Jenkins.tf
@@ -1,7 +1,10 @@
 resource "null_resource" "install_maven" {
-  provisioner "remote-exec" {
-    inline = [
-      "brew install maven",
-    ]
+  provisioner "local-exec" {
+    command = <<EOF
+      wget https://downloads.apache.org/maven/maven-3/3.8.2/binaries/apache-maven-3.8.2-bin.tar.gz
+      tar xzf apache-maven-3.8.2-bin.tar.gz
+      mv apache-maven-3.8.2 /opt/maven
+      echo 'export PATH=/opt/maven/bin:$PATH' >> ~/.bash_profile
+    EOF
   }
 }
