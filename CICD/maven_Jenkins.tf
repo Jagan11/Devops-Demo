7c3fe36382d3123e25b72541ecad43b1d1ed91ad
Jaganathan Dhanasekar <jagan.8981@gmail.com>
2023-06-16 18:21:55 +0800

    updateMv

--- CICD/maven_Jenkins.tf
+++ CICD/maven_Jenkins.tf
@@ -1,9 +1,9 @@
 resource "null_resource" "install_maven" {
   provisioner "local-exec" {
     command = <<EOF
-      wget https://downloads.apache.org/maven/maven-3/3.8.2/binaries/apache-maven-3.8.2-bin.tar.gz
-      tar xzf apache-maven-3.8.2-bin.tar.gz
-      mv apache-maven-3.8.2 /opt/maven
+      wget https://downloads.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz
+      tar xzf apache-maven-3.8.8-bin.tar.gz
+      mv apache-maven-3.8.8 /opt/maven
       echo 'export PATH=/opt/maven/bin:$PATH' >> ~/.bash_profile
     EOF
   }
